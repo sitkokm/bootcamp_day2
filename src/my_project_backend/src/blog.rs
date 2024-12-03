baaa1a54dc463d97695d7c51c6ef265581d4b3c0
Katarzyna Sitko <xxyzxx.xx.xx@gmail.com>
2024-12-03 16:04:43 +0000

    in progress

---
+++ src/my_project_backend/src/blog.rs
@@ -0,0 +1,18 @@
+#[derive(Clone)]
+pub struct Blog {
+    title: String,
+    date: u32,
+    content: String,
+    tags: Vec<String>
+}
+
+impl Blog {
+    pub fn new(title: String, date: u32, content: String, tags: Vec<String>) -> Self {
+        Self {
+            title,
+            date,
+            content,
+            tags
+        }
+    }
+}
--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,3 +1,23 @@
+use std::cell::RefCell;
+
+use crate::blog::Blog; //crate??
+
+mod blog;
+
+thread_local! {
+    static BLOGS: RefCell<Vec<Blog>> = RefCell::new(Vec::new());
+}
+
+#[ic_cdk::update] //what next??
+fn add_blog(title: String, date: u32, content: String, tags: Vec<String>) {
+    let blog = Blog::new(title, date, content, tags);
+    BLOGS.with(|blogs| blogs.borrow_mut().push(blog));
+}
+
+#[ic_cdk::query]
+fn get_blogs() -> Vec<Blog> {
+    BLOGS.with(|blogs| blogs.borrow().clone())
+}
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
